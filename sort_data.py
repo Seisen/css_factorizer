9c08ab92eba5afc154be1563dd700f1d67068d47
seisen <jochim6.2@hotmail.fr>
2021-01-14 17:04:20 +0100

    fixed

--- sort_data.py
+++ sort_data.py
@@ -1,38 +1,54 @@
-###temp
-with open("test.txt") as f:
-    inputs = f.read()
-
-
 class Sort:
     def __init__(self,datas):
-        self.datas = datas
+        self.datas = "".join([a.strip() for a in datas.splitlines()])
 
     def __repr__(self):
-        return self.run(self.datas)
+        return self.datas
 
-    def run(self,datas) -> dict:
-        result = {}
+    def get_dict(self) -> dict:
+        return self.result()
 
-        rules_sets = self.get_rules_sets(datas)
+    def result(self) -> dict:
+        rules_sets = self.get_rules_sets(self.datas)
+        resultat = {}
+
         for rule_set in rules_sets:
-            print(rule_set)
-            selectors = self.get_selectors(rule_set)
+            for selector in self.get_selectors(rule_set):
+                resultat[selector] = self.get_declaration_block(rule_set)
 
-            for selector in selectors:
-                result[selector] = self.get_declaration_block(rule_set)
+        return resultat
+
+    def is_exeption(self, datas: str) -> bool:
+        selectors = "".join(self.get_selectors(datas))
+        return "@" in selectors
+
+    def get_declaration_block(self, datas: str) -> list:
+        if not self.is_exeption(datas):
+            declaration_block = datas.split("{")[1]
+            result = [elem.strip() for elem in declaration_block.split(";")]
+        else:
+            result = datas.split("{")[1]
 
         return result
 
     @staticmethod
     def get_rules_sets(datas: str) -> list:
-        return datas.replace("\n", "").split("}")
+        i: int = -1
+        result: list = []
+        temp: str = ""
+
+        for data in datas:
+            temp += data
+
+            if data == "{":
+                i += 1
+            elif data == "}":
+                if i == 0:
+                    result.append(temp)
+                    temp = ""
+                i -= 1
+        return result
 
     @staticmethod
     def get_selectors(datas: str) -> list:
-        selectors = datas.split("{")[0]
-        return [elem.strip() for elem in selectors.split(",")]
-
-    @staticmethod
-    def get_declaration_block(datas: str) -> list:
-        declaration_block = datas.split("{")[1]
-        return [elem.strip() for elem in declaration_block.split(";")]
+        return datas.split("{")[0].split(",")
